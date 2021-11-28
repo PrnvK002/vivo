73115f8d39c7d107b9981e1621564cbc0ad7f181
PrnvK002 <codlearner12345@gmail.com>
2021-11-28 23:06:03 +0530

    Added some functionality ----change color of navabar in scroll ----change color of navlinks and added a one time top button -------------------------- by Pranav KV

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-var myNav = document.querySelector("header");
+var myNav = document.querySelector(".navbar");
+var myFont = document.querySelectorAll(".nav-link");
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    console.log(top);
-    if(top >= 100){
-        myNav.classList.add("active");
-        document.querySelector(".top_button").style.visibility="visible";
-    }
-    else{
-        myNav.classList.remove("active");  
-        document.querySelector(".top_button").style.visibility="hidden";
-
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 100) {
+      
+      myNav.classList.add("active");
+      document.querySelector(".top_button").style.visibility = "visible";
+      myFont.forEach((element)=>{
+          element.classList.add("change_color");
+      });
+  } else {
+     
+      myNav.classList.remove("active");
+      document.querySelector(".top_button").style.visibility = "hidden";
+      myFont.forEach((element)=>{
+          element.classList.remove("change_color");
+      });
+  }
 }
